ebfe22f6e0ba9be0881726e9ce96fb7cb1de6eeb
MedSveta <sveta1978medved@gmail.com>
2025-05-08 19:21:44 +0300

    PracticeFormPage

--- src/test/java/tests/PracticeFormTests.java
+++ src/test/java/tests/PracticeFormTests.java
@@ -1,16 +1,23 @@
 package tests;
 
+import dto.Student;
 import manager.AppManager;
 import org.testng.annotations.Test;
 import pages.FormsPage;
 import pages.HomePage;
+import pages.PracticeFormPage;
 
 public class PracticeFormTests extends AppManager {
 
     @Test
     public void studentRegFormPositiveTest(){
+        Student student = new Student("Vasya", "Vasilev", "vasyavasilev@gmail.com",
+                "male", "0123456789", "22 Dec 2010",
+                "Maths,Physics,Chemistry",
+                "sport", "", "street 1", "NCR", "Delhi");
         new HomePage(getDriver()).clickBtnForms();
         new FormsPage(getDriver()).clickBtnPracticeForm();
+        new PracticeFormPage(getDriver()).typePracticeForm(student);
 
     }
 }
