369d6bad877ceadf6236220d6f3f09f0b7da178f
MedSveta <sveta1978medved@gmail.com>
2025-05-12 19:07:54 +0300

    added Gender, Hobby, Date

--- src/main/java/pages/PracticeFormPage.java
+++ src/main/java/pages/PracticeFormPage.java
@@ -1,11 +1,17 @@
 package pages;
 
 import dto.Student;
+import enums.Gender;
+import enums.Hobbies;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.pagefactory.AjaxElementLocatorFactory;
+
+import java.util.List;
 
 public class PracticeFormPage extends BasePage {
     public PracticeFormPage(WebDriver driver) {
@@ -26,6 +32,8 @@ public class PracticeFormPage extends BasePage {
     WebElement fieldAddress;
     @FindBy(id = "dateOfBirthInput")
     WebElement fieldDateOfBirth;
+    @FindBy(id = "subjectsInput")
+    WebElement fieldSubjects;
     @FindBy(id = "react-select-3-input")
     WebElement inputState;
     @FindBy(id = "react-select-4-input")
@@ -38,8 +46,54 @@ public class PracticeFormPage extends BasePage {
         fieldName.sendKeys(student.getName());
         fieldLastName.sendKeys(student.getLastName());
         fieldEmail.sendKeys(student.getEmail());
+        typeGender(student.getGender());
         fieldMobile.sendKeys(student.getMobile());
         fieldAddress.sendKeys(student.getAddress());
+        //fieldDateOfBirth.sendKeys(student.getDateOfBirth());
+        typeDateOfBirth(student.getDateOfBirth());
+        typeSubjects(student.getSubject());
+        typeHobbies(student.getHobbies());
 
+    }
+    private void typeSubjects(String subjects){
+        fieldSubjects.click();
+        String[] arr = subjects.split(",");
+        for (String s: arr){
+            fieldSubjects.sendKeys(s);
+            fieldSubjects.sendKeys(Keys.ENTER);
+        }
+    }
+
+    private void typeGender(Gender gender) {
+        WebElement btnGender = driver.findElement(By.xpath(gender.getLocator()));
+        btnGender.click();
+    }
+
+    private void typeDateOfBirth(String dateOfBirth) {
+        fieldDateOfBirth.click();
+        String operationSystem = System.getProperty("os.name");
+        System.out.println(operationSystem);
+        if (operationSystem.startsWith("Win"))
+            fieldDateOfBirth.sendKeys(Keys.chord(Keys.CONTROL, "a"));
+        else if (operationSystem.startsWith("Mac"))
+            fieldDateOfBirth.sendKeys(Keys.chord(Keys.COMMAND, "a"));
+        fieldDateOfBirth.sendKeys(dateOfBirth);
+        fieldDateOfBirth.sendKeys(Keys.ENTER);
+    }
+
+    private void typeHobbies(List<Hobbies> hobbies){
+        for(Hobbies h : hobbies){
+            switch (h){
+                case MUSIC:
+                    driver.findElement(By.xpath(h.getLocator())).click();
+                    break;
+                case SPORTS:
+                    driver.findElement(By.xpath(h.getLocator())).click();
+                    break;
+                case READING:
+                    driver.findElement(By.xpath(h.getLocator())).click();
+                    break;
+            }
+        }
     }
 }
--- src/test/java/tests/PracticeFormTests.java
+++ src/test/java/tests/PracticeFormTests.java
@@ -1,20 +1,28 @@
 package tests;
 
 import dto.Student;
+import enums.Gender;
+import enums.Hobbies;
 import manager.AppManager;
 import org.testng.annotations.Test;
 import pages.FormsPage;
 import pages.HomePage;
 import pages.PracticeFormPage;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PracticeFormTests extends AppManager {
 
     @Test
     public void studentRegFormPositiveTest(){
+        List<Hobbies> hobbies = new ArrayList<>();
+        hobbies.add(Hobbies.MUSIC);
+        hobbies.add(Hobbies.SPORTS);
         Student student = new Student("Vasya", "Vasilev", "vasyavasilev@gmail.com",
-                "male", "0123456789", "22 Dec 2010",
+                Gender.FEMALE, "0123456789", "22 Dec 2010",
                 "Maths,Physics,Chemistry",
-                "sport", "", "street 1", "NCR", "Delhi");
+                hobbies, "", "street 1", "NCR", "Delhi");
         new HomePage(getDriver()).clickBtnForms();
         new FormsPage(getDriver()).clickBtnPracticeForm();
         new PracticeFormPage(getDriver()).typePracticeForm(student);
